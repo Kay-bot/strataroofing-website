9ca5641c9e0f07cd945af2bbfe3f63f8fc0d9b1c
KAY S <KS.KWANRUAN@GMAIL.com>
2020-09-23 00:28:23 +1000

    signin and signout

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -32,31 +32,44 @@ exports.signup = (req, res) => {
   });
 };
 
-// exports.signin = (req, res) => {
-//   const { email, password } = req.body;
-//   // check if user exist
-//   User.findOne({ email }).exec((err, user) => {
-//     if (err || !user) {
-//       return res.status(400).json({
-//         error: 'User with that email does not exist. Please signup',
-//       });
-//     }
-//   });
-//   // authenticate
-//   if (!user.authenticate(password)) {
-//     return res.status(400).json({
-//       error: 'Email or password do not match.',
-//     });
-//   }
-//   // generate a token and send to client
-//   const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET_KEY, {
-//     expiresIn: '1d',
-//   });
+exports.signin = (req, res) => {
+  const { email, password } = req.body;
+  // check if user exist
+  User.findOne({ email }).exec((err, user) => {
+    if (err || !user) {
+      return res.status(400).json({
+        error: 'User with that email does not exist. Please signup.',
+      });
+    }
+    // authenticate
+    if (!user.authenticate(password)) {
+      return res.status(400).json({
+        error: 'Email and password do not match.',
+      });
+    }
+    // generate a token and send to client
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: '1d',
+    });
 
-//   res.cookie('token', token, { expiresIn: '1d' });
-//   const { _id, username, name, email, role } = user;
-//   return res.json({
-//     token,
-//     user: { _id, username, name, email, role },
-//   });
-// };
+    res.cookie('token', token, { expiresIn: '1d' });
+    const { _id, username, name, email, role } = user;
+    return res.json({
+      token,
+      user: { _id, username, name, email, role },
+    });
+  });
+};
+
+exports.signout = (req, res) => {
+  res.clearCookie('token');
+  res.json({
+    message: 'Signout success',
+  });
+};
+
+exports.requireSignin = expressJwt({
+  secret: process.env.JWT_SECRET,
+  algorithms: ['HS256'], // added later
+  userProperty: 'auth',
+});
