4e03f54b1e3673035187efd5ca4aefc99eafec8f
KAY S <KS.KWANRUAN@GMAIL.com>
2020-09-22 21:19:27 +1000

    server setup

---
+++ backend/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+//bring routes
+const blogsRoutes = require('./routes/blog');
+// app
+const app = express();
+
+//db
+mongoose
+  .connect(process.env.DATABASE_CLOUD, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB Connected'));
+
+// middlewears
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+// cors
+if (process.env.NODE_ENV === 'development') {
+  app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
+}
+
+// routes middleware
+app.use('/api', blogsRoutes);
+
+// port
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
