9a8d1e8b814a0635842bda2e16b48feca1dd5b38
KAY S <KS.KWANRUAN@GMAIL.com>
2020-09-23 09:12:27 +1000

    frontend setup

---
+++ frontend/components/Header.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import { APP_NAME } from '../config';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  NavbarText,
+} from 'reactstrap';
+
+const Header = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <NavbarBrand href="/">{APP_NAME}</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="#">Home</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#">About</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#">What We Do</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="#">Blog</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Header;
---
+++ frontend/pages/index.js
@@ -0,0 +1,10 @@
+import Layout from '../components/Layout';
+const Index = () => {
+  return (
+    <Layout>
+      <h2>Hello World</h2>
+    </Layout>
+  );
+};
+
+export default Index;
