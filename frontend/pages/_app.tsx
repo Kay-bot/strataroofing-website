76f4a3fb9150c800a9dc46bd48200a55c3520f6e
KAY S <KS.KWANRUAN@GMAIL.com>
2020-09-23 14:10:17 +1000

    tsx

---
+++ frontend/pages/_app.tsx
@@ -0,0 +1,4 @@
+import 'react-bootstrap-carousel/dist/react-bootstrap-carousel.css';
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,8 +1,10 @@
 import Layout from '../components/Layout';
 import { Container } from 'reactstrap';
+import Carousel from '../components/Carousel';
 const Index = () => {
   return (
     <Layout>
+      <Carousel />
       <Container>
         <div>
           <h1>Hello World</h1>
