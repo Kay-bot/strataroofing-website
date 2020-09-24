cb4b50f669063774508e68ef75c5b84bcd08339e
KAY S <KS.KWANRUAN@GMAIL.com>
2020-09-24 22:40:32 +1000

    favicon

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,5 +1,14 @@
 import '../scss/global.scss';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Head>
+        <title>Strata Roofing</title>
+        <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
