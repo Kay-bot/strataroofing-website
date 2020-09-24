30129cc70dc7b9b6a08a6ac11050967034ff03e8
KAY S <KS.KWANRUAN@GMAIL.com>
2020-09-24 21:02:53 +1000

    our service page setup

---
+++ frontend/pages/our-services/index.tsx
@@ -0,0 +1,14 @@
+import Layout from '../../components/Layout';
+import { Container } from 'reactstrap';
+
+const Contact = () => {
+  return (
+    <Layout>
+      <Container>
+        <div>Our Services</div>
+      </Container>
+    </Layout>
+  );
+};
+
+export default Contact;
