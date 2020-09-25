e29add93daf24e71bbd84779a86442037000e308
KAY S <KS.KWANRUAN@GMAIL.com>
2020-09-25 21:56:07 +1000

    user and admin Dashboard

---
+++ frontend/pages/admin/index.tsx
@@ -0,0 +1,14 @@
+import Layout from '../../components/Layout';
+import { Container } from 'reactstrap';
+
+const AdminIndex = () => {
+  return (
+    <Layout>
+      <Container>
+        <h2>Admin Dashboard</h2>
+      </Container>
+    </Layout>
+  );
+};
+
+export default AdminIndex;
---
+++ frontend/pages/user/index.tsx
@@ -0,0 +1,13 @@
+import Layout from '../../components/Layout';
+import { Container } from 'reactstrap';
+const UserIndex = () => {
+  return (
+    <Layout>
+      <Container>
+        <h2>User Dashboard</h2>
+      </Container>
+    </Layout>
+  );
+};
+
+export default UserIndex;
