1e90769de075c6f3b1af1abdea1f677811a51279
KAY S <KS.KWANRUAN@GMAIL.com>
2020-09-27 17:04:58 +1000

    favicon

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -6,7 +6,24 @@ export default function App({ Component, pageProps }) {
     <div>
       <Head>
         <title>Strata Roofing</title>
-        <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="favicon-16x16.png"
+        />
+        {/* <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" /> */}
       </Head>
       <Component {...pageProps} />
     </div>
