38d536c2727f540e9f77c496ca4b1ca165818283
KAY S <KS.KWANRUAN@GMAIL.com>
2020-09-28 15:00:09 +1000

    blog backend

--- backend/controllers/category.js
+++ backend/controllers/category.js
@@ -38,7 +38,22 @@ exports.read = (req, res) => {
         error: errorHandler(err),
       });
     }
-    res.json(category);
+    // res.json(category);
+    Blog.find({ categories: category })
+      .populate('categories', '_id name slug')
+      .populate('tags', '_id name slug')
+      .populate('postedBy', '_id name')
+      .select(
+        '_id title slug excerpt categories postedBy tags createdAt updatedAt'
+      )
+      .exec((err, data) => {
+        if (err) {
+          return res.status(400).json({
+            error: errorHandler(err),
+          });
+        }
+        res.json({ category: category, blogs: data });
+      });
   });
 };
 
--- backend/controllers/tag.js
+++ backend/controllers/tag.js
@@ -15,7 +15,7 @@ exports.create = (req, res) => {
         error: errorHandler(err),
       });
     }
-    res.json({ data, message: 'Tag created' }); // dont do this res.json({ tag: data });
+    res.json({ data, message: 'Tag created' });
   });
 };
 
@@ -39,7 +39,22 @@ exports.read = (req, res) => {
         error: 'Tag not found',
       });
     }
-    res.json(tag);
+    // res.json(tag);
+    Blog.find({ tags: tag })
+      .populate('categories', '_id name slug')
+      .populate('tags', '_id name slug')
+      .populate('postedBy', '_id name')
+      .select(
+        '_id title slug excerpt categories postedBy tags createdAt updatedAt'
+      )
+      .exec((err, data) => {
+        if (err) {
+          return res.status(400).json({
+            error: errorHandler(err),
+          });
+        }
+        res.json({ tag: tag, blogs: data });
+      });
   });
 };
 
